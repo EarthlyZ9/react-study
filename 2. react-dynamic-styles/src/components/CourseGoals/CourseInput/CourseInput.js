bfd09a08cb35c0c6d8b17f249c100f6a3a027502
linda2927 <linda2927@naver.com>
2022-06-27 21:48:58 +0900

    add second project

--- 2. react-dynamic-styles/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 2. react-dynamic-styles/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
 const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+  const [enteredValue, setEnteredValue] = useState("");
 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
